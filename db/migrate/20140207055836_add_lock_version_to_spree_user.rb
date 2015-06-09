d985234ae9e0f57aed52b29b5f0090d551836d9b
pyro2927 <joseph@pintozzi.com>
2015-06-09 15:41:59 -0500

    Allowing migrations to work with other user tables

--- db/migrate/20140207055836_add_lock_version_to_spree_user.rb
+++ db/migrate/20140207055836_add_lock_version_to_spree_user.rb
@@ -1,5 +1,5 @@
 class AddLockVersionToSpreeUser < ActiveRecord::Migration
   def change
-    add_column :spree_users, :lock_version, :integer, default: 0, null: false unless column_exists?(:spree_users, :lock_version)
+    add_column Spree.user_class.table_name, :lock_version, :integer, default: 0, null: false unless column_exists?(:spree_users, :lock_version)
   end
 end
